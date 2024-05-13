2bd10c913836307cc329ad4cc16531d6ae9ef08e
Stefan Pascu <pascustefan30@yahoo.com>
2024-05-13 18:51:52 +0300

    add encode password

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import json
 import psycopg2 as ps
+import base64 as b64
 
 with open("config.json", "r") as f:
     config = json.loads(f.read())
+    config['password'] = b64.b64decode(config['password']).decode()
 
 
 with ps.connect(**config) as conn:
